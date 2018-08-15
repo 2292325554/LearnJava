baee36bb158f76148a29ef16a52d5e51b7878501
2292325554 <42413692+2292325554@users.noreply.github.com>
2018-08-15 23:24:44 +0800

    计算 求偶数之和

--- Circulate.java
+++ Circulate.java
@@ -3,7 +3,6 @@ package com.chenjian.test;
 public class Circulate {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int sum = 0;
 		for (int i = 2; i <= 100; i += 2) {
 			sum = sum + i;
